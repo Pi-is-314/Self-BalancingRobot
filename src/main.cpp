23520879d126d560a499e0f1dba0b3b38fe6d50a
jif <116607711+Pi-is-314@users.noreply.github.com>
2026-08-01 12:58:25 -0400

    Added the PID and the the library for the light array (PID Still needs to be mechanically tuned)

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 // Copyright (C) 2009 Mike McCauley
 // $Id: MultiStepper.pde,v 1.1 2011/01/05 01:51:01 mikem Exp mikem $
 
-
+ 
 /*
   MPU6050 Raw
 
@@ -20,7 +20,6 @@
 */
 
 //Libraries
-#include "I2Cdev.h"
 #include <AccelStepper.h>
 #include <Wire.h>
 #include <MPU6050_light.h>
@@ -30,20 +29,40 @@
 const int step1Pin = 26;
 const int dir1Pin = 25;
 
-const int step2Pin = 19;
-const int dir2Pin = 18;
+const int step2Pin = 17;
+const int dir2Pin = 16;
+
+const int DIN = 23;
+const int CS = 5;
+const int CLK = 18;
+ 
+const float kP = 1;
+const float kI = 0.1;
+const float kD = 0.1;
+
+const double idealAngle = 0.0;
+double currentAngle = 0.0;
+double totalError = 0.0;
+double previousError = 0.0;
+double speed = 0.0;
+
 
 AccelStepper stepper1(AccelStepper::DRIVER, step1Pin, dir1Pin);
 AccelStepper stepper2(AccelStepper::DRIVER, step2Pin, dir2Pin);
 
 
 
-//MPU Constants
+// MPU Constants
 MPU6050 mpu(Wire); //SDA = PIN 21, SCL = PIN 22
 unsigned long timer = 0;
 
+
+double calculatePID();
+
 void setup() {
-  Serial.begin(9600);
+  
+  Serial.begin(115200);
+  Serial.print("hello");
   Wire.begin();
   
   byte status = mpu.begin();
@@ -59,16 +78,16 @@ void setup() {
 
   stepper1.setMaxSpeed(800);
   stepper1.setAcceleration(400);
-  stepper1.moveTo(200);
+  stepper1.setSpeed(200);
 
   stepper2.setMaxSpeed(800);
   stepper2.setAcceleration(400);
-  stepper2.moveTo(200);
+  stepper2.setSpeed(200);
 }
 
 void loop() {
   mpu.update();
-  
+  currentAngle = mpu.getAngleY();
   if((millis()-timer)>10){ // print data every 10ms
 	Serial.print("X : ");
 	Serial.print(mpu.getAngleX());
@@ -78,10 +97,23 @@ void loop() {
 	Serial.println(mpu.getAngleZ());
 	timer = millis();  
   }
-  // if (stepper.distanceToGo() == 0) {
-  //   stepper.moveTo(-stepper.currentPosition());
-  // }
+  double speed = calculatePID();
+  stepper1.setSpeed(speed);
+  stepper2.setSpeed(speed);
   stepper1.run();
   stepper2.run();
 }
 
+double calculatePID() {
+  double error = idealAngle - currentAngle;
+  totalError += error;
+  double derivative = error - previousError;
+  double output = kP * error + kI * totalError + kD * derivative;
+  previousError = error;
+  return output;
+}
+
+void setSpeed(double speed){
+  stepper1.setSpeed(speed);
+  stepper2.setSpeed(speed);
+}
